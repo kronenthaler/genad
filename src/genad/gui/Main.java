c093ad89da77d337faf62a57af1b6f16b0c2f3b1
kronenthaler <kronenthaler@4e85e6c8-e837-49c3-9e53-9323525de7d3>
2008-09-28 20:27:49 +0000

    splash and about for version 1.5
    git-svn-id: file:///home/kronenthaler/work/genad-repo@58 4e85e6c8-e837-49c3-9e53-9323525de7d3

--- src/genad/gui/Main.java
+++ src/genad/gui/Main.java
@@ -77,7 +77,7 @@ public class Main extends javax.swing.JFrame implements View{
         aboutItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-        setTitle("GenAd v1.0");
+        setTitle("GenAd v1.5");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setState(JFrame.MAXIMIZED_BOTH);
         addWindowListener(new java.awt.event.WindowAdapter() {
